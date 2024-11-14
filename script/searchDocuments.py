cb2e77f169f2a9ea22d3ef61960ca897e22a4ae3
mjawadhaider <m.jawadhaider03@gmail.com>
2024-11-14 08:31:29 +0500

    Improvements in indexing and searching

--- script/searchDocuments.py
+++ script/searchDocuments.py
@@ -1,58 +1,78 @@
 import os
-from datetime import datetime
 
 class searchEngine:
     def __init__(self):
         self.indexes = {}
-        self.stopWords = {'the', 'is', 'and', 'in', 'to', 'of', 'a', 'with', 'for', 'on', 'by'}
+        # common Stop words and verb's suffixes to be filtered from text
+        self.stopWords = {'the', 'is', 'and', 'in', 'to', 'of', 'a', 'with', 'for', 'on', 'by', 'are', 'an', 'as', 'that'}
+        self.verbSuffixes = {'ing', 'ed', 'ate', 'ify', 'ize', 'en', 'fy'}
 
+    # Add fileName in the dict against its indexedWord 
+    def addIndexInDict(self, indexWord, value):
+        if indexWord not in self.indexes:
+            self.indexes[indexWord] = [value]
+        elif value not in self.indexes[indexWord]:
+            self.indexes[indexWord].append(value)
+
+    # Take filePath as parameter and returns it's content
     def readFile(self, filePath):
         try:
             with open(filePath, 'r') as file:
                 content = file.read()
                 return content
         except FileNotFoundError:
-            print(f'The file {filePath} does not exist.')
+            displayMsg(f'The file {filePath} does not exist.')
         
         return None
 
-    def insertInDictionary(self, fileName, contentWords):
-        # Remove .txt if any word has
+    # This method read's the documentsFolderPath, reads the files content and creates title and content's indexes
+    def createIndexes(self, documentsFolderPath):
+        try:
+            for filename in os.listdir(documentsFolderPath):
+                if filename.endswith('.txt'):
+                    filePath = os.path.join(documentsFolderPath, filename)
+                    fileContent = self.readFile(filePath)
+
+                    if not fileContent:
+                        continue
+
+                    self.createTitleAndContentsIndex(filename, fileContent)
+            return True
+        except FileNotFoundError:
+            displayMsg(f'The Directory {documentsFolderPath} does not exist.')
+            return False
+
+    # Take fileName and it's content as parameters and creates its index in the dictionary
+    def createTitleAndContentsIndex(self, fileName, contentWords):
+        # Remove .txt if any fileName has
         if 'txt' in fileName:
             fileName = fileName.split('.')[0]
 
-        for wordInFileName in fileName.split() + contentWords:
-            wordInFileName = wordInFileName.lower()
+        filteredFileNamesList = self.filterImportantWords(fileName)
+        filteredContentWordsList = self.filterImportantWords(contentWords)
 
-            if '.' in wordInFileName:
-                wordInFileName = wordInFileName.replace('.', '')
-            if ',' in wordInFileName:
-                wordInFileName = wordInFileName.replace(',', '')
+        for indexWord in filteredFileNamesList + filteredContentWordsList:
+            self.addIndexInDict(indexWord, fileName)
 
-            if wordInFileName not in self.indexes:
-                self.indexes[wordInFileName] = [fileName]
-            elif fileName not in self.indexes[wordInFileName]:
-                self.indexes[wordInFileName].append(fileName)
-
-    def createIndexes(self, documentsFolderPath):
-        for filename in os.listdir(documentsFolderPath):
-            if filename.endswith('.txt'):
-                filePath = os.path.join(documentsFolderPath, filename)
-                fileContent = self.readFile(filePath)
-
-                if fileContent:
-                    fileContent = self.filterImportantWords(fileContent)
-
-                self.insertInDictionary(filename, fileContent)
-
+    # This method take text as parameter and remove stop words and words with verb suffixes
     def filterImportantWords(self, text):
         wordsList = []
         for word in text.split():
-            if word not in self.stopWords:
-                wordsList.append(word)
+            word = word.lower().strip(',.')
+
+            # Check if the word is a stop word
+            if word in self.stopWords:
+                continue 
+
+            # Check if the word ends with a verb suffix
+            if any(word.endswith(suffix) for suffix in self.verbSuffixes):
+                continue
+
+            wordsList.append(word)
 
         return wordsList
 
+    # This method take query as parameter and returns a list of related file names
     def searchDocuments(self, query):
         if len(self.indexes) == 0:
             displayMsg('Create Indexes to search a document!')
@@ -69,6 +89,7 @@ class searchEngine:
         
         return results
 
+    # Display Index and its values
     def lookupIndexes(self):
         if len(self.indexes) == 0:
             return displayMsg('Create indexes to look up!')
@@ -81,6 +102,7 @@ class searchEngine:
             print()
         print('-------------------------------------')
 
+    # Display results of the searched query
     def displayResult(self, query, result):
         print('\n-------------------------------------')
         print('Searched Query: ', query)
@@ -92,7 +114,7 @@ class searchEngine:
                 print('\t', index + 1, file)
         print('-------------------------------------')
 
-
+# Helper function to display messages
 def displayMsg(msg):
     print('\n-------------------------------------')
     print(msg)
@@ -104,30 +126,30 @@ if __name__ == '__main__':
 
     while True:
         print('\nMenu:')
-        print('1. Create Indexes')
-        print('2. Lookup Indexes')
-        print('3. Search FileName')
-        print('4. Exit')
+        print('1. Change Documents Directory Path (default: ./documents)')
+        print('2. Create Indexes')
+        print('3. Lookup Indexes')
+        print('4. Search FileName')
+        print('5. Exit')
 
-        choice = input('Enter your choice (1-4): ')
+        choice = input('Enter your choice (1-5): ')
 
-        startTime = datetime.now()
         if choice == '1':
-            searchEngineInstance.createIndexes(documentsFolderPath)
-            displayMsg('Indexes created successfully!')
+            documentsFolderPath = input('\nEnter Documents Directory: ')
+            displayMsg('Documents Directory Path updated successfully!')
         elif choice == '2':
-            searchEngineInstance.lookupIndexes()
+            indexCreated = searchEngineInstance.createIndexes(documentsFolderPath)
+            if indexCreated:
+                displayMsg('Indexes created successfully!')
         elif choice == '3':
+            searchEngineInstance.lookupIndexes()
+        elif choice == '4':
             query = input('\nEnter Query: ')
-            startTime = datetime.now()
             results = searchEngineInstance.searchDocuments(query)
             if results:
                 searchEngineInstance.displayResult(query, results)
-        elif choice == '4':
+        elif choice == '5':
             print('Exiting program.')
             break
         else:
-            displayMsg('Invalid choice. Please enter a number between 1 and 4.')
-
-        timeDifference = (datetime.now() - startTime).total_seconds() * 10**3
-        print("Execution time of program is: ", timeDifference, "ms")
+            displayMsg('Invalid choice. Please enter a number between 1 and 5.')
